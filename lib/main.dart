77c49ed4621295b75cb856043adeb7928e0700e6
kijang <wahyu.creator911@gmail.com>
2020-10-13 13:02:30 +0700

    update animation size and color on flutter

--- lib/main.dart
+++ lib/main.dart
@@ -31,12 +31,18 @@ class _MyHomePageState extends State<MyHomePage> {
   double status;
   Color color;
 
+  double sizeStart, sizeEnd, size;
+
   @override
   void initState() {
     super.initState();
 
     this.status = 0;
     this.color = Colors.blue;
+
+    this.size = 0;
+    this.sizeStart = 0;
+    this.sizeEnd = 500;
   }
 
   @override
@@ -49,30 +55,70 @@ class _MyHomePageState extends State<MyHomePage> {
           height: double.infinity,
           color: Colors.grey[40],
           child: Center(
-            child: Column(
+            child: Stack(
+              fit: StackFit.expand,
               children: [
-                TweenAnimationBuilder(
-                  tween: ColorTween(begin: Colors.white, end: this.color),
-                  duration: const Duration(milliseconds: 500),
-                  builder: (context, value, child) {
-                    return ColorFiltered(
-                      colorFilter: ColorFilter.mode(value, BlendMode.modulate),
-                      child: child,
-                    );
-                  },
-                  child: Image.asset("assets/flutter_logo.png"),
+                Center(
+                  child: TweenAnimationBuilder(
+                    tween: ColorTween(begin: Colors.white, end: this.color),
+                    duration: const Duration(milliseconds: 500),
+                    builder: (context, value, child) {
+                      return ColorFiltered(
+                        colorFilter:
+                            ColorFilter.mode(value, BlendMode.modulate),
+                        child: child,
+                      );
+                    },
+                    child: TweenAnimationBuilder(
+                      tween: SizeTween(
+                          begin: Size(100, 100),
+                          end: Size(
+                              100 +
+                                  ((this.sizeEnd - this.sizeStart) * this.size),
+                              100 +
+                                  ((this.sizeEnd - this.sizeStart) *
+                                      this.size))),
+                      builder: (context, valueSize, child) {
+                        return Container(
+                          width: valueSize.width,
+                          height: valueSize.height,
+                          child: child,
+                        );
+                      },
+                      child: Image.asset("assets/flutter_logo.png"),
+                      duration: Duration(
+                        milliseconds: 200,
+                      ),
+                    ),
+                  ),
                 ),
-                Text("color white to red"),
-                Slider.adaptive(
-                  value: status,
-                  onChanged: (value) {
-                    setState(() {
-                      status = value;
-                      color = Color.lerp(Colors.blue, Colors.green, value);
-                    });
-                  },
-                ),
-                Text("Size"),
+                Positioned(
+                  bottom: 0,
+                  child: Column(
+                    children: [
+                      Text("color white to red"),
+                      Slider.adaptive(
+                        value: status,
+                        onChanged: (value) {
+                          setState(() {
+                            status = value;
+                            color =
+                                Color.lerp(Colors.blue, Colors.green, value);
+                          });
+                        },
+                      ),
+                      Text("Size"),
+                      Slider.adaptive(
+                        value: this.size,
+                        onChanged: (value) {
+                          setState(() {
+                            this.size = value;
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                )
               ],
             ),
           ),
